18e5007dc040986cb12a29abea6bbf9c481c3af7
Naysa-Gerard <mendozamarcgerard@gmail.com>
2024-08-06 15:10:44 +0800

    second

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,20 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use app\Models\User;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->only('EMAIL_ADD', 'USER_ID');
+        $request->validate([
+            'EMAIL_ADD' => 'required|email',
+            'USER_ID' => 'required',
+        ]);
 
-        if (Auth::attempt($credentials)) {
-            // Authentication passed
-            return response()->json(['message' => 'Login successful'], 200);
-        } else {
-            // Authentication failed
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
+
+        // Authentication passed
+        Auth::login($user); // Optional: Log in the user if you are using sessions
+        return response()->json(['message' => 'Login successful'], 200);
+    
     }
 }
 
